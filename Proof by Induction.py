883440c1c04a90a87bead15d56e15c349cd0745f
mateowartelle <mateow@192.168.1.7>
2018-02-15 11:37:53 -0600

    + Finished All Setup of Proof
    - Need to add Logic to math expansion and factoring

--- Proof by Induction.py
+++ Proof by Induction.py
@@ -50,14 +50,29 @@ def get_top(top):
     else:
         topdata = top
         return topdata
-        
+
+def base_case_Phase3(top, Random_variable, middle, evaluating):
+    for element in middle:
+        if element == top:
+            middle = middle.replace(top, Random_variable)
+    for secondelement in evaluating:
+        if secondelement == top:
+            evaluating = evaluating.replace(top, Random_variable)
+    return middle, evaluating
+
+def inductive_hypothesis_plus_one(top, Random_variable, middle, evaluating):
+    temp = top
+    top = top.replace(top, Random_variable + " + 1")
+    middle = middle.replace(temp, "("+top+")")
+    evaluating = evaluating.replace(temp, "("+top+")")
+    return top, evaluating, middle
+           
 def random_generate(in_list):
     x = string.ascii_lowercase
     random_ = ''.join(list((random.choice(x) for num in range(1))))
     while random_ in in_list:
         random_ = ''.join(list((random.choice(x) for num in range(1))))
     return random_
-    print(random_)
           
 def summation(top, bottom, middle, evaluating, basecase):
     for stringdata in top:
@@ -77,10 +92,9 @@ def summation(top, bottom, middle, evaluating, basecase):
     evaluated_data.insert(2, middle)
     evaluated_data.insert(3, evaluating)
     evaluated_data.insert(4, basecase)
-    print(used_variables)
     return evaluated_data
 
-evaluated_data = summation("k", "i=1", "2i - 1", "k^2", 1)
+evaluated_data = summation("j", "i=1", "2i - 1", "j", 1)
 top = evaluated_data[0].strip()
 bottom = evaluated_data[1].strip().strip()
 middle = frac_to_Tex(evaluated_data[2]).strip()
@@ -89,27 +103,33 @@ basecase = evaluated_data[4]
 
 #Setup
 Html_file.write("Prove by Mathmatical Induction" )
-middle = frac_to_Tex(middle)
-evaluating = frac_to_Tex(evaluating)
 Phase1 = """$$\sum_{%s}^%s %s = %s $$""" % (bottom, top, middle, evaluating)
 Html_file.write(Phase1)
 
 # Base Case
-Html_file.write("Base case n = {}".format(basecase))
-evaluating, middle = base_case_top_bottom(top, bottom, middle, evaluating, basecase)
-Phase2 = """$$\sum_{%s}^%s %s = %s $$""" % (bottom, basecase, middle, evaluating)
+temp = get_top(top)
+Html_file.write("Base case {} = {}".format(temp, basecase))
+evaluating_base_case, middle_base_case = base_case_top_bottom(top, bottom, middle, evaluating, basecase)
+Phase2 = """$$\sum_{%s}^%s %s = %s $$""" % (bottom, basecase, middle_base_case, evaluating_base_case)
 Html_file.write(Phase2)
+Html_file.write("<center>" + middle_base_case + " = " + evaluating_base_case + "</center>" + "<br>")
 
 #Inductive Hypothesis
 Random_variable = random_generate(used_variables)
 topdata = get_top(top)
 Html_file.write("Assume for some {} = {}".format(Random_variable, topdata))
 
-#Phase3 = """$$\sum_{%s}^%s %s = %s $$""" % (bottom, basecase, middle, evaluating)
+middle_phase3, evaluating_phase3 = base_case_Phase3(top, Random_variable, middle, evaluating)
+Phase3 = """$$\sum_{%s}^%s %s = %s $$""" % (bottom, Random_variable, middle_phase3, evaluating_phase3)
+Html_file.write(Phase3)
+
+Html_file.write("Show true for {} + 1".format(Random_variable))
+top_phase4, evaluating_phase4, middle_phase4 = inductive_hypothesis_plus_one(top, Random_variable, middle, evaluating)
+Phase4 = """$$\sum_{%s}^{%s} %s = %s $$""" % (bottom, top_phase4, middle_phase4, evaluating_phase4)
+Html_file.write(Phase4)
+
+
 
 
 Html_file.write("""</p></body></html>""")
 Html_file.close()
-
-    
-
