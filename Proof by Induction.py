b92275e331257257f2ef35e7f8cf85a8d6b0070f
mateowartelle <mateow@192.168.1.7>
2018-02-15 02:01:45 -0600

    + Closer
    + Decided to Scrap the Styles.css idea and added MathJax.js for summation / fraction / exponent notation
    - Need to add type checks (ie k = 1, k, k(k+1), k^2)
    - Need to Add Logic expansion + distributing variables

--- Proof by Induction.py
+++ Proof by Induction.py
@@ -3,142 +3,113 @@ import random
 
 used_variables = []
 evaluated_data = []
-top =""
-bottom = ""
-middle = ""
-evaluating = ""
-basecase = 0
-topeval = ""
-bottomeval = ""
 
 Html_file= open("index.html","w")
-Css_file = open("styles.css", "w")
-Html_file.write("""<link rel="stylesheet" href="styles.css">""")
+Html_file.write("""<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>MathJax example</title>
+  <script type="text/javascript" async
+  src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.2/MathJax.js?config=TeX-MML-AM_CHTML">
+</script>
+</head>
+<body>
+<p>
+""")
 
-def generate_top_bottom(top, bottom):
-    Css_file.write("""p {
-    height: 50px;
-    line-height: 50px;
-    }
-    span {
-        position: relative;
-        font-size: 2.5em;
-        display: inline-block;
-        line-height: .7em;
-        vertical-align: middle;
-    }
-    span::before {
-        font-size: 12px;
-        display: block;
-        position absolute;
-        left: 0;
-        top: 0;
-        content: "%s";
-        width: 22px;
-        text-align: center;
-    }
-    span::after {
-        font-size: 12px;
-        display: block;
-        position absolute;
-        left: 0;
-        bottom: 0;
-        content: "%s";
-        width: 27px;
-        text-align: center;
-    }
-    """ % (top, bottom))
-    
+def frac_to_Tex(frac):
+    for i in frac:
+      if i == "/":
+          topeval = frac.split("/")[0]
+          bottomeval = frac.split("/")[1]
+          return "\\frac{%s}{%s}" % (topeval, bottomeval)
+    return frac
 
-def generate_html_page(top, bottom, middle, evaluating, topeval, bottomeval):
-    Html_file.write("""   <p>
-        <span>&Sigma;</span>
-        {} = <sup>{}</sup>&frasl;<sub>{}</sub>
-    </p>""".format(middle, topeval, bottomeval))
+def base_case_top_bottom(top, bottom, middle, evaluating, basecase):    
+    if len(top) > 1:
+        topdata = top.split("=")
+        topdata = topdata[0]
+    else:
+        topdata = top
+    if len(bottom) > 1:
+        bottomdata = bottom.split("=")
+        bottomdata = bottomdata[0]
+    else:
+        bottomdata = bottom
+    evaluating = evaluating.replace(topdata, "("+str(basecase)+")")
+    middle = middle.replace(bottomdata, "("+str(basecase)+")")
+    return evaluating, middle
 
-    Css_file.write("""p {
-    height: 50px;
-    line-height: 50px;
-    }
-    span {
-        position: relative;
-        font-size: 2.5em;
-        display: inline-block;
-        line-height: .7em;
-        vertical-align: middle;
-    }
-    span::before {
-        font-size: 12px;
-        display: block;
-        position absolute;
-        left: 0;
-        top: 0;
-        content: "%s";
-        width: 22px;
-        text-align: center;
-    }
-    span::after {
-        font-size: 12px;
-        display: block;
-        position absolute;
-        left: 0;
-        bottom: 0;
-        content: "%s";
-        width: 27px;
-        text-align: center;
-    }
-    """ % (top, bottom))
-
+def get_top(top):
+    if len(top) > 1:
+        topdata = top.split("=")
+        topdata = topdata[0]
+        return topdata
+    else:
+        topdata = top
+        return topdata
+        
+def random_generate(in_list):
+    x = string.ascii_lowercase
+    random_ = ''.join(list((random.choice(x) for num in range(1))))
+    while random_ in in_list:
+        random_ = ''.join(list((random.choice(x) for num in range(1))))
+    return random_
+    print(random_)
+          
 def summation(top, bottom, middle, evaluating, basecase):
-    topeval = ""
-    bottomeval = ""
     for stringdata in top:
         if stringdata.isalpha():
             used_variables.append(stringdata)
     for stringdata in bottom:
         if stringdata.isalpha():
-            used_variables.append(stringdata)
-            
+            used_variables.append(stringdata)   
     top = top.replace(" ", "")
     bottom = bottom.replace(" ", "")
     if len(top) > 1:
         topdata = top.split("=")[1]
     if len(bottom) > 1:
         bottomdata = bottom.split("=")[1]
-    for i in evaluating:
-      if i == "/":
-          topeval = evaluating.split("/")[0]
-          bottomeval = evaluating.split("/")[1]
     evaluated_data.insert(0, top)
     evaluated_data.insert(1, bottom)
     evaluated_data.insert(2, middle)
     evaluated_data.insert(3, evaluating)
     evaluated_data.insert(4, basecase)
-    evaluated_data.insert(5, topeval)
-    evaluated_data.insert(6, bottomeval)
+    print(used_variables)
     return evaluated_data
- 
+
+evaluated_data = summation("k", "i=1", "2i - 1", "k^2", 1)
+top = evaluated_data[0].strip()
+bottom = evaluated_data[1].strip().strip()
+middle = frac_to_Tex(evaluated_data[2]).strip()
+evaluating = frac_to_Tex(evaluated_data[3]).strip()
+basecase = evaluated_data[4]
+
+#Setup
+Html_file.write("Prove by Mathmatical Induction" )
+middle = frac_to_Tex(middle)
+evaluating = frac_to_Tex(evaluating)
+Phase1 = """$$\sum_{%s}^%s %s = %s $$""" % (bottom, top, middle, evaluating)
+Html_file.write(Phase1)
+
 # Base Case
-evaluated_data = summation("n", "i=1", "i", "n(n+1)/2", 0)
-generate_html_page(evaluated_data[0], evaluated_data[1], evaluated_data[2], evaluated_data[3], evaluated_data[5], evaluated_data[6])
+Html_file.write("Base case n = {}".format(basecase))
+evaluating, middle = base_case_top_bottom(top, bottom, middle, evaluating, basecase)
+Phase2 = """$$\sum_{%s}^%s %s = %s $$""" % (bottom, basecase, middle, evaluating)
+Html_file.write(Phase2)
 
 #Inductive Hypothesis
-unused_variable = random.choice(string.ascii_letters).lower()
-Html_file.write("<br>")
-Html_file.write("Assume for some {} = {}".format(unused_variable, evaluated_data[0]))
-Html_file.write("""<p>
-        <span>&Sigma;</span>
-        {} = <sup>{}</sup>&frasl;<sub>{}</sub>
-    </p>""".format(evaluated_data[2], evaluated_data[5], evaluated_data[6]))
+Random_variable = random_generate(used_variables)
+topdata = get_top(top)
+Html_file.write("Assume for some {} = {}".format(Random_variable, topdata))
+
+#Phase3 = """$$\sum_{%s}^%s %s = %s $$""" % (bottom, basecase, middle, evaluating)
 
 
-#Proving variable + 1
-##Html_file.write("<br>")
-##Html_file.write("Show true for {}+1".format(used_variables[0]))
-##
-##summation("i = 1", "n", "i", "n(n+1)/2", 0)   
+Html_file.write("""</p></body></html>""")
 Html_file.close()
-Css_file.close()
 
     
 
